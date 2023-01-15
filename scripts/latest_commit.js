7842a8c6441d4e6569d0e9d3ad1acace7e74770a
Oscar Hudson <hudso009@gmail.com>
2023-01-15 02:51:23 +0000

    carrd

--- scripts/latest_commit.js
+++
@@ -1,9 +0,0 @@
-fetch('https://api.github.com/repos/oscie57/oscie57.github.io/commits?per_page=1')
-.then(resp => resp.json())
-.then(resp => {
-    document.getElementById('commit-author').innerHTML = resp[0].commit.committer.name
-    document.getElementById('commit-message').innerHTML = resp[0].commit.message
-    document.getElementById('commit-link').href = resp[0].parents[0].html_url
-    document.getElementById('commit-id').innerHTML = resp[0].sha.slice(0, 6)
-    document.getElementById('commit-date').innerHTML = resp[0].commit.committer.date.replace("-", "/").replace("-", "/").replace("T", ", ").replace("Z", " UTC")
-})
