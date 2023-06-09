a0bc1feb3cb41313a9eb647e82361bf69fcf518e
Oscar Hudson <hudso009@gmail.com>
2023-06-10 00:19:06 +0100

    fix latest_commit.js

--- scripts/latest_commit.js
+++ scripts/latest_commit.js
@@ -1,11 +1,14 @@
-fetch('https://api.github.com/repos/oscie57/oscie57.github.io/commits?per_page=1')
+fetch('https://api.github.com/repos/oscie57/oscie57.github.io/branches/carrd')
 .then(resp => resp.json())
 .then(resp => {
-    console.log(resp)
-    
-    document.getElementById('commit-author').innerHTML = resp[0].commit.committer.name
-    document.getElementById('commit-message').innerHTML = resp[0].commit.message
-    document.getElementById('commit-link').href = resp[0].parents[0].html_url
-    document.getElementById('commit-id').innerHTML = resp[0].sha.slice(0, 6)
-    document.getElementById('commit-date').innerHTML = resp[0].commit.committer.date.replace("-", "/").replace("-", "/").replace("T", ", ").replace("Z", " UTC")
+    document.getElementById('commit-author').innerHTML = resp.commit.committer.login
+    document.getElementById('commit-message').innerHTML = resp.commit.commit.message
+    document.getElementById('commit-link').href = resp.commit.html_url
+    document.getElementById('commit-id').innerHTML = resp.commit.sha.slice(0, 6)
+    document.getElementById('commit-date')
+        .innerHTML = resp.commit.commit.committer.date
+        .replace("-", "/")
+        .replace("-", "/")
+        .replace("T", ", ")
+        .replace("Z", " UTC")
 })
