bef20e10c17a871490e543f9008fe1a012d2b14c
Oscar Hudson <hudso009@gmail.com>
2023-07-23 02:08:31 +0100

    Update latest_commit.js

--- scripts/latest_commit.js
+++ scripts/latest_commit.js
@@ -4,7 +4,7 @@ fetch('https://api.github.com/repos/oscie57/oscie57.github.io/branches/carrd')
     document.getElementById('commit-author').innerHTML = resp.commit.committer.login
     document.getElementById('commit-message').innerHTML = resp.commit.commit.message
     document.getElementById('commit-link').href = resp.commit.html_url
-    document.getElementById('commit-id').innerHTML = resp.commit.sha.slice(0, 6)
+    document.getElementById('commit-id').innerHTML = resp.commit.sha.slice(0, 7)
     document.getElementById('commit-date')
         .innerHTML = resp.commit.commit.committer.date
         .replace("-", "/")
