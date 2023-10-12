086c678e351bc3b874d8309243961217f087a9af
Oscar Hudson <hudso009@gmail.com>
2023-10-12 03:30:52 +0100

    WE OUT HERE‼️

--- scripts/visitor_count.js
+++ scripts/visitor_count.js
@@ -1,5 +1,24 @@
-    fetch('https://api.oscie.net/views')
-        .then(resp => resp.json())
-        .then(resp => {
-                document.getElementById('viewcount').innerHTML = resp
-    })
+fetch('https://api.oscie.net/views?site=oscie.net')
+
+    .then(resp => resp.json())
+    .then(resp => {
+        var count = resp['oscie.net'].toString();
+        var zeroes = '';
+
+        if (count.length == 0) {
+            zeroes = '00000';
+        } else if (count.length == 1) {
+            zeroes = '0000';
+        } else if (count.length == 2) {
+            zeroes = '000';
+        } else if (count.length == 3) {
+            zeroes = '00';
+        } else if (count.length == 4) {
+            zeroes = '0';
+        } else if (count.length == 5) {
+             zeroes = '';
+        }
+
+        document.getElementById('count').innerHTML = '<span id="greycount">' + zeroes + '</span>' + count;
+
+})
