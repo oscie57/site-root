fac19f42f9dee91c92b27591276d636818d1c1ed
Oscar Hudson <hudso009@gmail.com>
2023-10-12 02:57:03 +0100

    Update visitor_count.js

--- scripts/visitor_count.js
+++ scripts/visitor_count.js
@@ -1,4 +1,4 @@
-    fetch('http://api.oscie.net/views')
+    fetch('https://api.oscie.net/views')
         .then(resp => resp.json())
         .then(resp => {
                 document.getElementById('viewcount').innerHTML = "55555"
